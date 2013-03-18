45c2c86e521eec7fdda5eba6de9157f05ccce0c2
denisdbv <denisdbv@gmail.com>
2013-03-18 17:33:09 +0400

    Отображение AboutViewController с блоками
    Исправлено нарушение закона Деметры
    
    Упрощен метод доступа к иконке и текстовым полям с помощью билдера и указателей
    
    Добавлен класс по созданию кнопки

--- Brandymint/AppDelegate.m
+++ Brandymint/AppDelegate.m
@@ -20,7 +20,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
     ownViewController = [[OwnViewController alloc] initWithNibName:@"OwnViewController" bundle:[NSBundle mainBundle]];
---
+++ Brandymint/Controllers/AboutViewController/AboutViewController.m
@@ -0,0 +1,64 @@
+//
+//  AboutViewController.m
+//  Brandymint
+//
+//  Created by DenisDbv on 18.03.13.
+//  Copyright (c) 2013 denisdbv@gmail.com. All rights reserved.
+//
+
+#import "AboutViewController.h"
+#import "BlockView.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+@synthesize blocs;
+
+- (id)initAboutController:(NSArray*)blocArray
+{
+    self = [super initWithNibName:@"AboutViewController" bundle:[NSBundle mainBundle]];
+    if (self) {
+        
+        self.blocs = blocArray;
+        
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return UIInterfaceOrientationIsLandscape(interfaceOrientation);
+}
+
+-(void) viewWillAppear:(BOOL)animated
+{
+    self.view.backgroundColor = [UIColor clearColor];
+    
+    for(NSInteger loop = 0; loop < blocs.count; loop++)
+    {
+        Bloc *bloc = [blocs objectAtIndex: (NSUInteger)loop ];
+        
+        BlockView * blockView = (BlockView*)[self.view viewWithTag: loop+1 ];
+        [blockView fillView:bloc];
+    }
+}
+
+
+
+
+@end
