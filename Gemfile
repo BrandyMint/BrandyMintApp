338e382764e1965d9e2466b5f2ff945bf182405c
Danil Pismenny <danil@orionet.ru>
2013-03-20 21:36:19 +0400

    Подключил Gemfile

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+#gem 'liftoff'
+gem 'ocunit2junit'
+gem 'cocoapods', '>=0.17.0.rc5'
--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,30 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-xcodebuild -workspace Brandymint.xcworkspace -scheme BrandymintTests -sdk iphonesimulator6.1  -configuration Debug clean build
+
+bundle install
+bundle exec pod repo update
+bundle exec pod install
+
+#DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
+
+
+xcodebuild -workspace Brandymint.xcworkspace -scheme BrandymintTests -sdk iphonesimulator6.1  -configuration Debug clean build 2>&1 | bundle exec ocunit2junit
+
+# Reads
+# http://9elements.com/io/index.php/continuous-integration-of-ios-projects-using-jenkins-cocoapods-and-kiwi/
+# http://www.raingrove.com/2012/03/28/running-ocunit-and-specta-tests-from-command-line.html
+
+# brew install ios-sim
+# RUN_APPLICATION_TESTS_WITH_IOS_SIM=YES 
+
+# Launch application using ios-sim and set up environment to inject test bundle into application
+# Source: http://stackoverflow.com/a/12682617/504494
+ 
+#if [ "$RUN_APPLICATION_TESTS_WITH_IOS_SIM" = "YES" ]; then
+    #test_bundle_path="$BUILT_PRODUCTS_DIR/$PRODUCT_NAME.$WRAPPER_EXTENSION"
+    #environment_args="--setenv DYLD_INSERT_LIBRARIES=/../../Library/PrivateFrameworks/IDEBundleInjection.framework/IDEBundleInjection --setenv XCInjectBundle=$test_bundle_path --setenv XCInjectBundleInto=$TEST_HOST"
+    #ios-sim launch $(dirname $TEST_HOST) $environment_args --args -SenTest All $test_bundle_path
+    #echo "Finished running tests with ios-sim"
+#else
+    #"${SYSTEM_DEVELOPER_DIR}/Tools/RunUnitTests"
+#fi
