a29efaddbe92882643148bb9769a1526af831b81
denisdbv <denisdbv@gmail.com>
2013-03-27 13:33:52 +0400

    Удален nss файл стиля

--- Brandymint/AppDelegate.m
+++ Brandymint/AppDelegate.m
@@ -21,7 +21,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [NUISettings initWithStylesheet:@"Brandymint"];
+    //[NUISettings init];
   
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
